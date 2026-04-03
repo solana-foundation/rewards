a2f28540c2448eba79f92c17a21ecab0f069b1db
Jo D <dev-jodee@users.noreply.github.com>
2026-04-03 10:55:52 -0400

    refactor(rewards-program): remove continuous and points instruction flows

--- program/src/instructions/mod.rs
+++ program/src/instructions/mod.rs
@@ -1,10 +1,8 @@
-pub mod continuous;
 pub mod definition;
 pub mod direct;
 pub mod emit_event;
 pub mod impl_instructions;
 pub mod merkle;
-pub mod points;
 
 pub use definition::*;
 pub use impl_instructions::*;
--- program/src/instructions/points/revoke_points/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod accounts;
-pub mod data;
-pub mod processor;
-
-pub use crate::instructions::impl_instructions::RevokePoints;
-pub use accounts::*;
-pub use data::*;
-pub use processor::*;
